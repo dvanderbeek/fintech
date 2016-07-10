24f7a3b0579286d1276ccaf7ccee49c4111dd7ae
David Van Der Beek <earlynovrock@gmail.com>
2016-07-10 18:39:12 -0500

    add benchmark

--- lib/fintech.rb
+++ lib/fintech.rb
@@ -1,6 +1,7 @@
 Dir.glob(File.join('./lib', 'fintech', '**', '*.rb'), &method(:require))
 require 'ostruct'
 require 'pry'
+require 'benchmark'
 
 module Fintech
 end
---
+++ lib/fintech/benchmark.rb
@@ -0,0 +1,18 @@
+require 'benchmark'
+
+module Fintech
+  class Benchmark
+    def self.run
+      "Time take n: #{(time * 1000).round(2)} milliseconds; Ending Balance: #{@stats.last.ending_balance.round(2)} cents."
+    end
+
+    def self.time
+      ::Benchmark.realtime {
+        l = Loan.new(amount_cents: 10000_00, rate: 0.1, term: 24, funding_date: Date.new(2015, 12, 1))
+        l.pay_installments
+        l.add_payment(amount_cents: 100_00, date: Date.new(2015, 12, 20), apply_to_future: true)
+        @stats = l.daily_stats
+      }
+    end
+  end
+end
